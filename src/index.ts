d69e1ed4c191b5482bfd0887685d9e729b40147f
Elecshen <elecshen@yandex.ru>
2024-07-13 18:39:35 +0600

    .

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,23 @@ import dirRoutes from "./routes/dir";
 import { swaggerOptions, swaggerUiOptions } from "./config/swagger";
 import { migrate } from "./utils/migrate";
 import { authRoutes } from "./routes/auth";
+import cors from "@fastify/cors";
 
 migrate("up");
 
 const fastify = Fastify({ logger: true });
 
+fastify.register(cors, {
+  origin: "*",
+  methods: "*",
+});
 fastify.register(import("@fastify/swagger"), swaggerOptions);
 fastify.register(import("@fastify/swagger-ui"), swaggerUiOptions);
 fastify.register(fastifyMultipart);
 fastify.register(authRoutes);
 fastify.register(dirRoutes, { prefix: "/folder" });
 
-fastify.listen({ port: 3000 }, (err, address) => {
+fastify.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
